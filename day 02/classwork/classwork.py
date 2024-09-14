b4693fd8a8b2b6f997d3b1f548862030e6c3b893
widar670 <itzxdio@gmail.com>
2024-09-14 23:47:31 +0500

    Add files via upload

---
+++ day 02/classwork/classwork.py
@@ -0,0 +1,12 @@
+name = "andria"
+surname = "sharashidze"
+print("andria")
+print("sharashidze")
+
+player = 15
+player_speed = 0.90
+print("--------------")
+
+
+print(player-5)
+print(player_speed-10)
---
+++ day 02/homework/homework.py
@@ -0,0 +1,27 @@
+score = 50
+print(score)
+
+health = 100
+print(health)
+
+attacking_points = 25
+defence_points = 20
+print(attacking_points,defence_points)
+
+math_score = 7,
+english_score = 9.5
+science_score = 6 ,
+chemistry_score = 6,
+history_Score = 10,
+
+print(math_score,english_score,science_score,chemistry_score,history_Score)
+
+name = "andria"
+surname = "sharashidze,"
+
+month = "september"
+date_of_birth = 7,2010
+age = ("age")
+age1 = 14
+print(name, surname, "age" + " " + str(age1,),"september", 7,2010)
+
